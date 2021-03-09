ab70a64c35268eeceec71eb0cc202ef6cd564122
Namdev <akashjadhavafi41@gmail.com>
2021-03-09 16:49:25 +0530

    joke generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState ,useEffect } from 'react';
+
+const API_URl="https://api.icndb.com/jokes/random";
 
 function App() {
+
+  const [joke, setJoke] = useState('');
+
+  useEffect(() => {
+    GenerateJoke();
+
+  }, [])
+
+  const GenerateJoke = () => {
+    fetch(API_URl)
+    .then(res =>res.json())
+    .then(data=>setJoke(data.value.joke)); //this is for the fetching the  joke From the APi
+  }
+  console.log({joke})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     
+    <div className="Box">
+        <div>
+          <h2>Joke Generator</h2>
+          <p dangerouslySetInnerHTML={{__html:joke}}/>
+
+          <button onClick={GenerateJoke}>Get New Joke</button>
+              
+        </div>
+        <div className="Header">
+        
+        <a href="https://twitter.com/">Twitter</a>
+      
+      </div>
     </div>
+    
+    
   );
+ 
 }
 
 export default App;
